21f0a402648066c24469ad9fd1a1a89f1145a247
gitmalong <gitmalong@posteo.af>
2016-05-12 18:05:37 +0200

    SKDamagableWithDamageEvents extension, cleanup
    - Added SKDamagableWithDamageEvents extension
    - Removed synchedSubtractHealthPoints, stopAction & addAction methods

--- SKDamagableSpriteNode.swift
+++ SKDamagableSpriteNode.swift
@@ -1,93 +1,81 @@
+extension NSDate: Comparable { }
+
+public func ==(lhs: NSDate, rhs: NSDate) -> Bool {
+    return lhs.isEqualToDate(rhs)
+}
+
+public func <(lhs: NSDate, rhs: NSDate) -> Bool {
+    return lhs.compare(rhs) == .OrderedAscending
+}
+
 //
 //  Damageable.swift
 //
-/* 
- 
- Protocol & extension that adds "damagable" functionality to a SKNode
- 
- - Node gets life points that can be substracted
- - onDeath() is triggered when the node "dies"
- - Support for nodes that have children. Use it when all of them should be able to get damaged but the life points (of the parent node) should only be substracted once for each damaging event (i.e. when the hero's body parts are all children and both feets are contacted by the same fire)
-
- Usage:
- 1) Create a class that conforms to DamagableNode for your parent node and a class that conforms to DamageChild for all your parent nodes children
- 
- 2) If you want to apply damage to a node check if the node conforms to "Damagable" and apply the damage via syncedSubstractHealthPoints(number:Int, uniqueDamageEventID:String) or use getAffectedSKNode() to access the parent/master node do what ever you want to do
-
-  Created by gitmalong on 20.04.16.
-*/
 
 import Foundation
 import SpriteKit
 import UIKit
 import AudioToolbox
 
-protocol SKDamagable {
-    func substractHealthPoints(number:Int)
-    func syncedSubstractHealthPoints(number:Int, uniqueDamageEventID:String) -> Void
-    func getHealthPoints() -> Int
-    func addAction(action:SKAction,withKey:String) -> Void
-    func stopAction(actionKey:String) -> Void
-    func getAffectedSKNode() -> SKNode
-}
-
-/* If your Damagable node has children that could also be damaged 
-    add them as a node class that conforms to this protocol. if multiple body parts 
- run into a damaging event, the health point substraction will only be applied once to your Damagable parent node
+/**
+ 
+ Protocol & extension that adds "damagable" functionality to a SKNode
+ 
+ - Node gets life points that can be substracted
+ 
+ - onDeath() is triggered when the node "dies"
+ 
+ - Support for nodes that have children. Use it when all of them should be able to get damaged but the life points (of the parent node) should only be substracted once for each damaging event (i.e. when the hero's body parts are all children and both feets are contacted by the same fire)
+ 
+ Usage:
+ 1) Create a class that conforms to DamagableNode for your parent node and a class that conforms to DamageChild for all your parent nodes children
+ 
+ 2) If you want to apply damage to a node check if the node conforms to "Damagable" and apply the damage via subtractHealthPoints() or use getAffectedSKNode() to access the parent/master node and do what ever you want
  
  */
-protocol SKDamagableChild:SKDamagable {
-    var parentNode:SKDamageableNode! { get set }
+protocol SKDamagable {
+    
+    /// Subtract health points
+    func subtractHealthPoints(number:Int)
+
+    /// Returns health points
+    func getHealthPoints() -> Int
+    
+    /// Returns self if it is the father node, otherwise it should return the parent node
+    func getAffectedSKNode() -> SKNode
+    
 }
 
-/* this class redirects all damaging events to the parent node in order to
- make sure that each damage event is only applied once to your node group
+protocol SKDamagableChild:SKDamagable {
+    var parentNode:SKDamageableMaster! { get set }
+}
 
- */
+/// Class redirects all damaging events to the parent node
 extension SKDamagableChild where Self:SKNode  {
-    
-    /* redirects health point substraction to parent */
-    func syncedSubstractHealthPoints(number:Int, uniqueDamageEventID:String) {
-        parentNode.syncedSubstractHealthPoints(number, uniqueDamageEventID: uniqueDamageEventID)
+
+    /// Substract health points from the parents node call this only when you are sure the same damage event is not called by other children of your parent
+    func subtractHealthPoints(number: Int) {
+        parentNode.subtractHealthPoints(number)
     }
     
-    /* substract health points from the parents node
-       call this only when you are sure the same damage event
-       is not called by other children of your parent
-     */
-    func substractHealthPoints(number: Int) {
-        parentNode.substractHealthPoints(number)
-    }
-    
-    /* redirect to parent */
-    func addAction(action:SKAction,withKey:String) {
-        parentNode.addAction(action,withKey: withKey)
-    }
-    
-    /* redirect to parent */
-    func stopAction(actionKey:String) {
-        parentNode.stopAction(actionKey)
-    }
-    
-    /* redirect to parent */
+    /// Returns health points of master node
     func getHealthPoints() -> Int {
         return parentNode.getHealthPoints()
     }
     
-    /* return master node */
+    /// Returns master node
     func getAffectedSKNode() -> SKNode {
         return parentNode.getAffectedSKNode()
     }
+
 }
 
 class SKDamagableSpriteNode:SKSpriteNode, SKDamagableChild {
-    var parentNode:SKDamageableNode!
+    var parentNode: SKDamageableMaster!
     
-    init(parentNode:SKDamageableNode, texture:SKTexture) {
+    init(parentNode:SKDamageableMaster, texture:SKTexture) {
         self.parentNode = parentNode
-        
         super.init(texture: texture, color: UIColor.whiteColor(), size: texture.size())
-        
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -96,7 +84,7 @@ class SKDamagableSpriteNode:SKSpriteNode, SKDamagableChild {
 }
 
 /* Master node */
-protocol SKDamageableNode:class, SKDamagable {
+protocol SKDamageableMaster: class, SKDamagable {
     var healthPoints:Int { get set }
 
     func updateHealthPoints(number:Int) -> Void
@@ -105,23 +93,9 @@ protocol SKDamageableNode:class, SKDamagable {
 }
 
 /* Master node */
-extension SKDamageableNode where Self:SKNode  {
-    
-    /* If your physics body that gets damaged has multiple body party
-     we want the damagable nodes children apply the damaging only to the
-     parent (this class). to prevent applying the same damage event multiple
-     times we execute them as action with an unique id. multiple events get 
-     overwritten
-     */
-    func syncedSubstractHealthPoints(number:Int, uniqueDamageEventID:String) {
-        let substractBlock = SKAction.runBlock( {
-          self.substractHealthPoints(number)
-        })
-        
-        self.runAction(substractBlock, withKey: uniqueDamageEventID)
-    }
-    
-    func substractHealthPoints(number:Int) {
+extension SKDamageableMaster where Self:SKNode  {
+
+    func subtractHealthPoints(number:Int) {
         updateHealthPoints(healthPoints-number)
     }
     
@@ -140,14 +114,7 @@ extension SKDamageableNode where Self:SKNode  {
     func getHealthPoints()->Int {
         return self.healthPoints
     }
-    
-    func stopAction(actionKey:String) {
-        self.removeActionForKey(actionKey)
-    }
-    
-    func addAction(action: SKAction, withKey:String) {
-        self.runAction(action, withKey: withKey)
-    }
+
     
     func getAffectedSKNode() -> SKNode {
         return self as SKNode
@@ -155,11 +122,32 @@ extension SKDamageableNode where Self:SKNode  {
     
 }
 
-/* Updates an SKLabelNode with new health points & vibrates */
-protocol DamageableUserCharacter:SKDamageableNode {
-    var healthPointsLabelNode:SKLabelNode { get }
+
+/// Protocol that adds SKDamageEvents to SKDamagable
+protocol SKDamagableWithDamageEvents:class, SKDamagable, SKDamagableEventManager {
+
+    /// Adds and applies SKDamageEvent and subtracts health of node
+    func addAndApplyDamageEvent(event: SKDamageEvent) -> Void
     
-    func afterUpdateHealthPoints()
+}
+
+/// Default implementation for protocol SKDamagableWithDamageEvents
+extension SKDamagableWithDamageEvents {
+    
+    /// Adds and applies SKDamageEvent and subtracts health of node
+    func addAndApplyDamageEvent(event: SKDamageEvent) -> Void {
+        event.applied = true
+        event.appliedTime = NSDate().timeIntervalSince1970
+        damageEvents.append(event)
+        self.subtractHealthPoints(event.damagePoints)
+        print(self.getHealthPoints())
+    }
+
+}
+
+/// Updates an SKLabelNode with new health points & vibrates
+protocol DamageableUserCharacter:SKDamageableMaster, SKDamagableWithDamageEvents {
+    var healthPointsLabelNode:SKLabelNode { get }
 }
 
 extension DamageableUserCharacter {
